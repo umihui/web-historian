2a0b2cc00fa225719dfd260dd77e4b443790713e
Hack Reactor Student <student@hackreactor.com>
2017-09-15 21:29:55 -0700

    refactor

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,40 +5,55 @@ var httphelper = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-
   if (req.method === 'GET') {
-    fs.readFile('/Users/student/code/hrsf82-web-historian/web/public/index.html', 'utf8', function(err, data) {
-    //console.log('ERROR!!!', err);
+    fs.readFile('/Users/student/code/hrsf82-web-historian/web/public/index.html', 'utf8',
+      function(err, data) {
       res.writeHead(200, httphelper.headers);
       res.end(data);
-    });
+      }
+    );
   }
 
   if (req.method === 'POST') {
     var body = '';
-
     req.on('data', function(data) {
       body += data;
     });
-    // var input = body.split('=')[1];
+
 
     req.on('end', function() {
+      console.log(body);
       var input = body.split('=')[1];
-      // archive.isUrlInList(input, function(result) {
-      //   if (!result) {
-      archive.addUrlToList(input, function() {
-        res.writeHead(302, httphelper.headers);
-        res.end('');
+      archive.isUrlInList(input, function(result) {
+        if (result) {
+          console.log('isUrlInList: true', input, result);
+          fs.readFile('/Users/student/code/hrsf82-web-historian/web/public/loading.html', 'utf8',
+            function(err, data) {
+              res.writeHead(200, httphelper.headers);
+              res.end(data);
+            });
+        } else {
+          console.log('isUrlInList: false', input, result);
+          archive.isUrlArchived(input, function(result) {
+
+            if (result) {
+              console.log('isUrlArchived: true', input, result);
+              res.writeHead(302, httphelper.headers);
+              res.end('');
+              // archive.addUrlToList(input, function() {
+
+              // });
+            } else {
+              console.log('isUrlArchived: false', input, result);
+              archive.addUrlToList(input, function() {
+                res.writeHead(302, httphelper.headers);
+                res.end('');
+              });
+            }
+          });
+        }
       });
-      //   }
-
-        //url not in the list : url was Archived or because it's new url
-        //if result is false: add url to sites.txt and retrun loading page
-        //esle return files
-
-      //console.log('post archive', archive.isUrlInList(input, function(result) {console.log('WE KNOW ABOUT CALLBACKS:', result)}));
     });
-
   }
 
   if (req.method === 'OPTIONS') {
